4ad45415a58a80e12107703cfc421f8f6284decc
kleber lima <limkf2009@gmail.com>
2025-10-30 03:10:52 +0000

    Atualiza configurações do servidor para usar a porta 5508; adiciona novo arquivo funcoes.js com funções matemáticas básicas e refatora arquivos existentes para melhor organização.

---
+++ aulas/javascript/funcoes.js
@@ -0,0 +1,118 @@
+// funcoes sao blocos de codigo que podem
+// ser reaproveitados ao longo da execucao deo programa
+// caracteristicas
+// podem ser nomeados ou nao
+// podem receber parametros ou nao
+// podem retornar valores ou nao
+
+
+// funcao pode receber arrays objestos e outras funcoes como parametros
+
+// function dizOLA() {
+//     alert.log('ola!');
+// }
+
+// function dizOLApessoa(nome  ) {
+//     alert (`nao seja timido ${nome}, diga ola!`);
+// }
+// const jogadores = ['ana', 'bia', 'carlos'];
+// const frutas = ['banana', 'maca', 'laranja'];
+
+// function formatatexto(lista){
+//     for(let texto in lista)
+//         console.log(texto.toUpperCase())
+// }
+
+// function formatatexto(lista){
+// }
+// //formatatexto(jogadores)
+
+function adicao(n1, n2) {
+    return n1 + n2;
+    }
+
+    const resultado = adicao(5, 3);
+    console.log(`Resultado da adicao: ${resultado}`) // 8
+
+// const resultadoadicao = adicao(5, 3);
+// console.log(resultadoadicao); // 8
+
+function subtracao(n1, n2) {
+    return n1 - n2;
+    }
+
+    const resultadosubtracao = subtracao(5, 3);
+    console.log(`Resultado da subtração: ${resultadosubtracao}`) // 2
+
+// const resultadosubtracao = subtracao(5, 3);
+// console.log(resultadosubtracao); // 8
+
+function multiplicacao(n1, n2) {
+    return n1 * n2;
+    }
+    const resultadomultiplicacao = multiplicacao(5, 3);
+    console.log(`Resultado da multiplicação: ${resultadomultiplicacao}`) // 15
+
+// const resultadomultiplicacao = multiplicacao(5, 3);
+// console.log(resultadomultiplicacao); // 8
+
+function divisao(n1, n2) {
+    return n1 / n2;
+    }
+    const resultadodivisao = divisao(5, 3);
+    console.log(`Resultado da divisão: ${resultadodivisao}`) // 1.6666666666666667
+
+
+// const resultadodivisao = divisao(5, 3);
+// console.log(resultadodivisao); // 8
+
+// function formatatexto(lista){
+//     for(let testo of lista)
+//         console.log(TextDecoder.toUpperCase());
+
+// }
+
+
+// function formatatexto(lista){
+//     for(let testo of lista)
+//         console.log(texto.toUpperCase());
+//     //texto.tolowerCase() > testo tudo para minuscula
+//     //texto.trim()  > texto espacos em brancos
+//     //texto.join ("outra coisa ") > une elementos do array em uma string + junta texto 
+
+// }
+
+
+
+// jogadores.forEach(dizOLApessoa);
+// frutas.forEach(dizOLApessoa);
+
+
+// // // invocando a funcao
+// // dizOLA(); // ola!
+// // dizOLApessoa('joao'); // nao seja timido joao, diga ola!
+// // dizOLApessoa('maria'); // nao seja timido maria, diga ola!
+// // dizOLApessoa('ana'); // nao seja timido ana, diga ola!
+
+
+// // // funcao com parametros
+
+
+
+
+// // function saudacao(nome) {
+// //     console.log('ola ' + nome + '!');
+// // }
+
+// // saudacao('joao'); // ola joao!
+// // saudacao('maria'); // ola maria!
+
+// // // funcao com retorno
+
+// // function soma(a, b) {
+// //     return a + b;
+// // }
+
+// // var resultado = soma(3, 5);
+// // console.log(resultado); // 8
+
--- aulas/javascript/objetos.js
+++ aulas/javascript/objetos.js
@@ -1,90 +1,90 @@
-// dados simples: numericos, strings, boolean..
-// array: [1,2,3, "ana"]
-//objetos: {propriedade: valor}
-// funcoes;  functiion somar () {} => {}
+// // dados simples: numericos, strings, boolean..
+// // array: [1,2,3, "ana"]
+// //objetos: {propriedade: valor}
+// // funcoes;  functiion somar () {} => {}
 
-// object literal = vou criar objeto direto - objetcos literal 
-// irar criar uma chave e associar a uma propriedade valor
+// // object literal = vou criar objeto direto - objetcos literal 
+// // irar criar uma chave e associar a uma propriedade valor
 
-// const pessoa = { 
+// // const pessoa = { 
 
-//     propriedade1: "valor1",
-//     // key: value
-//     nome: "Ana",
-//     idade: 29,
-//     profissao: "Desenvolvedora",
-//     possuiFaculdade: true       
+// //     propriedade1: "valor1",
+// //     // key: value
+// //     nome: "Ana",
+// //     idade: 29,
+// //     profissao: "Desenvolvedora",
+// //     possuiFaculdade: true       
 
+// // }
+
+// const perfil = {
+//     nome: "João",
+//     idade: 32,
+//     profissao: "Designer",
+//     possuiFaculdade: false,
+//     solteiro: true
+//     roupas : [ "camisetas", "calças", "vestidos" ]      
 // }
 
-const perfil = {
-    nome: "João",
-    idade: 32,
-    profissao: "Designer",
-    possuiFaculdade: false,
-    solteiro: true
-    roupas : [ "camisetas", "calças", "vestidos" ]      
-}
-
-// perfil.nome = "Carlos" // alterando valor da propriedade nome
-// perfil.idade = 28 // alterando valor da propriedade idade   
+// // perfil.nome = "Carlos" // alterando valor da propriedade nome
+// // perfil.idade = 28 // alterando valor da propriedade idade   
 
 
-console.log(Object.entries(perfil)) // mostra todas as chaves e valores do objeto em forma de array
-console.log(Object.keys(perfil)) // mostra todas as chaves e valores do objeto em forma de array
-console.log(Object.values(perfil)) // mostra todas as chaves e valores do objeto em forma de array
+// console.log(Object.entries(perfil)) // mostra todas as chaves e valores do objeto em forma de array
+// console.log(Object.keys(perfil)) // mostra todas as chaves e valores do objeto em forma de array
+// console.log(Object.values(perfil)) // mostra todas as chaves e valores do objeto em forma de array
 
 
 
 
-// for(let chave in perfil) {
-//     console.log(chave)
-//     console.log(perfil[chave])
-// }
+// // for(let chave in perfil) {
+// //     console.log(chave)
+// //     console.log(perfil[chave])
+// // }
 
-// let dadosPerfil = [];
+// // let dadosPerfil = [];
 
-// for(let dados in perfil) {
-//     dadosPerfil.push( perfil + ": " + perfil[dados] )
-// }
+// // for(let dados in perfil) {
+// //     dadosPerfil.push( perfil + ": " + perfil[dados] )
+// // }
 
-// console.log(dadosPerfil.join("\n")) // join une os elementos do array em uma string, separando por que eu quiser        
+// // console.log(dadosPerfil.join("\n")) // join une os elementos do array em uma string, separando por que eu quiser        
 
-// alert(dadosPerfil.join("\n"))
+// // alert(dadosPerfil.join("\n"))
 
 
 
 
 
-// for(let dados in perfil) {
-//     console.log(dados)
-// }
+// // for(let dados in perfil) {
+// //     console.log(dados)
+// // }
 
 
-// for(let i=0; < perfil.lenght; i++ ) {
-//     console.log(perfil.roupas[i])
-// }
+// // for(let i=0; < perfil.lenght; i++ ) {
+// //     console.log(perfil.roupas[i])
+// // }
 
-// // imprimindo o objeto inteiro
-
-
+// // // imprimindo o objeto inteiro
 
 
 
-// console.log(pessoa)
-// console.log(perfil)
 
-// // como acessar as propriedades do objeto
-// // dot notation (notacao de ponto)
 
-// console.log(pessoa.nome) // Ana
-// console.log(perfil.idade) // 32
+// // console.log(pessoa)
+// // console.log(perfil)
 
-// // atribuicao dinamica
-// // perfil.roupas = [ "camisetas", "calças", "vestidos" ] // adicionando nova propriedade ao objeto
-// // console.log(perfil.roupas)  
+// // // como acessar as propriedades do objeto
+// // // dot notation (notacao de ponto)
 
-// // bracket notation (notacao de colchetes)
+// // console.log(pessoa.nome) // Ana
+// // console.log(perfil.idade) // 32
+
+// // // atribuicao dinamica
+// // // perfil.roupas = [ "camisetas", "calças", "vestidos" ] // adicionando nova propriedade ao objeto
+// // // console.log(perfil.roupas)  
+
+// // // bracket notation (notacao de colchetes)
 
 
 
